bfda08b3abd20c01995d2e4637b48aaa6b0dc61d
Francisco <fjhuertru@gmail.com>
2026-09-07 18:45:13 -0600

    Issue 2:General Folder for Backend added. New Folders added for Backend controllers, repositories, services & utils. Added the Spring Files

---
+++ Backend/src/test/java/com/equipo3/dogalert/DogalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo3.dogalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
